c23c876c43e7e3259c0ba79596f13499a4a1b7c5
nicolas <nico.0107g@gmail.com>
2025-05-07 23:05:12 -0500

    AnalizadorMain

---
+++ src/main/java/com/mycompany/analizadorlexicomoca/AnalizadorLexicoMoca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.analizadorlexicomoca;
+
+/**
+ *
+ * @author PC
+ */
+public class AnalizadorLexicoMoca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
